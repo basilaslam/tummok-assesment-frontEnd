4128ed35b9e8a012c131e146385b0f9f2b8796d7
Basil Aslam N P <64439076+basilaslam@users.noreply.github.com>
2023-06-07 06:45:29 +0100

    react-router-dom v5 to v6

--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store/store';
-import Register from './routes/Register.tsx';
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register  from './routes/Register.tsx'
 import Login from './routes/Login.tsx';
+import './index.css'
+import { Provider } from 'react-redux';
+import store from './store/store.ts'
 import Redirect from './routes/Redirect.tsx';
 import Home from './routes/Home.tsx';
-import './index.css';
+import NotFound from './routes/NotFound.tsx';
 
-const styles = {
+ const styles = {
   label: 'block text-gray-700 text-sm font-bold pt-2 pb-1',
   field:
     'bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none',
   button:
-    'bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600',
+    ' bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600',
   errorMsg: 'text-red-500 text-sm',
   form: 'w-full sm:w-[400px] m-7 sm:m-0',
   title: 'text-center font-bold',
-  error: 'text-sm text-red-700'
-};
+  error: ' text-sm text-red-700'
+}
 
-const router = (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/login" element={<Login styles={styles} />} />
-    <Route path="/register" element={<Register styles={styles} />} />
-    <Route path="/OAuthRedirecting" element={<Redirect />} />
-  </Routes>
-);
+const router = createBrowserRouter([
+  
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+  {
+    path: '/',
+    element: <Home />,
+  },
+    {
+      path: '/login',
+      element: <Login styles={styles}/>,
+    },
+    {
+      path: '/register',
+      element: <Register styles={styles}/>,
+    },
+    {
+      path: '/OAuthRedirecting',
+      element: <Redirect />,
+    }
 
-ReactDOM.render(
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>{router}</Router>
+    <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
---
+++ src/routes/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+	<div className=' font-semibold text-lg'>Page Not Found</div>
+  )
+}
+
+export default NotFound
